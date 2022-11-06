59ad81138ab5b57cd8eeadce2f190da653a8e40a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-11-06 20:26:38 +0100

    Moved logic to SectionCard components
    Each Card.jsx component was creating a modal logic for itself. So
    moved all this logic and markup to a SectionCard component wich wrapp
    all the cards.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,36 +1,20 @@
-import { useState } from "react";
 import { List } from "../list/List.jsx";
 import { Button } from "../button/Button.jsx";
-import { Modal } from "../modal/Modal.jsx";
 
-export function Card({ data }) {
-  const [modal, setModal] = useState(false);
+export function Card({ data, toggleModal }) {
   const { logo, type, cost, period, benefits, buttonText } = data;
-  const toggleModal = () => setModal(!modal);
-
   return (
-    <>
-      <article>
-        <div className="card__logo">
-          <img src={logo} alt="Card logo" />
-        </div>
-        <div className="card__intro">
-          <p>{type}</p>
-          <p>{cost}</p>
-          <p>{period}</p>
-        </div>
-        <List data={benefits} />
-        <Button handleClick={toggleModal} data={buttonText} />
-      </article>
-      {modal ? (
-        <Modal>
-          <p>
-            Thanks for your interest in <span>{type}</span> plan. Write your
-            email and we will reach you soon.
-          </p>
-          <Button handleClick={toggleModal} data={"Close"} />
-        </Modal>
-      ) : null}
-    </>
+    <article>
+      <div className="card__logo">
+        <img src={logo} alt="Card logo" />
+      </div>
+      <div className="card__intro">
+        <p>{type}</p>
+        <p>{cost}</p>
+        <p>{period}</p>
+      </div>
+      <List data={benefits} />
+      <Button handleClick={toggleModal} data={buttonText} />
+    </article>
   );
 }
