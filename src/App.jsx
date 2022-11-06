b09bca92735d82c32a4ef5ce0b660c3c3c5dca94
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-11-06 18:50:36 +0100

    Imported card and dat and made initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Card } from "./components/card/Card.jsx";
+import { data } from "./data.js";
 
 function App() {
-  return <h1>Rendering</h1>;
+  return (
+    <>
+      <h1>Rendering</h1>
+      {data.map(function (element, idx) {
+        return <Card key={idx} data={element} />;
+      })}
+    </>
+  );
 }
 
 const container = document.getElementById("root");
